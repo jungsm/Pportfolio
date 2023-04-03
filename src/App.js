5b492026d9d6dc981e79f671f4f64fca4ee70f35
Jsm <seonmunjeong95@gmail.com>
2023-04-03 18:25:10 +0900

    애니

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import { Animated } from 'react-native';
-import React from 'react';
+
 import './App.css';
-class SomeComponent extends React.Component {
-  state = {
-    animation: new Animated.Value(0)
-  }
-  // ...
-}
+
 function App() {
   return (
     <section id="wrap">
@@ -14,13 +8,11 @@ function App() {
       <div className='container'>
       <section className='main_banner'>
         <div className='main_banner_left'>
-          <div className=''>
-        {/*   <img className='img_banner' src="img/main.png"/> */}
-          </div>
+          <img className='img_banner' src="img/main.png"/>
         </div>
         <div className='main_banner_right'>
           <div className='right_textbox'>
-            <p className='title'>해와 달처럼 조화<p/> </p>
+            <p className='title'>태양과 달처럼 조화로운<p/> </p>
             <div className="main_button_box">
             <a href='https://mango2.notion.site/008cc17e31554d1cbf85c6c993dcd561'><button>Notion</button></a>
             <a href='https://github.com/jungsm?tab=repositories'><button>Git Hub</button></a>
@@ -33,7 +25,7 @@ function App() {
     </section>
     </div>
     <img src='img/img.png'className='shape01' ></img>
-    <img src='img/banner_shape02.png'className='shape02' ></img>
+    <img src='img/banner_shape02.png'className='shape02'  ></img>
     <img src='img/banner_shape03.png'className='shape03' ></img>
     <img src='img/banner_shape04.png'className='shape04' ></img>
     <img src='img/banner_shape05.png'className='shape05' ></img>
