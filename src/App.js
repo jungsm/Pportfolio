255aa84ca3ba1a2a97193564d2e7a4590c5aece2
Jsm <seonmunjeong95@gmail.com>
2023-04-04 18:22:01 +0900

    expo start123

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
-
+import React from "react";
+import './rest.css'
 import './App.css';
-
+import './font/font.css'
 function App() {
   return (
     <section id="wrap">
+     
       <div className='banner_img'>
       <div className='container'>
+      <section className='header container'>
+        <div className='menu_nav'>
+          <div className='logo'></div>
+          <div className='menu_item'>
+            <ul className='menu_drop'>
+              <li><a>Home</a></li>
+              <li><a>About</a></li>
+              <li><a>Portfolio</a></li>
+            </ul>
+          </div>
+        </div>
+      </section>
       <section className='main_banner'>
         <div className='main_banner_left'>
         {/*   <img className='img_banner' src="img/main.png"/> */}
         </div>
         <div className='main_banner_right'>
           <div className='right_textbox'>
-            <p className='title'>해와 달처럼 조화롭게 코드....<p/> </p>
+            <p className='title'>해와 달처럼<br></br> 조화로운 코드를<p/> </p>
             <div className="main_button_box">
             <a href='https://mango2.notion.site/008cc17e31554d1cbf85c6c993dcd561'><button>Notion</button></a>
             <a href='https://github.com/jungsm?tab=repositories'><button>Git Hub</button></a>
-            <a href=''><button>이력서</button></a>
             </div>
         </div>
         </div>  
     </section>
-    <section>
-    </section>
-    </div>
+    </div >
     <img src='img/img.png'className='shape01' ></img>
     <img src='img/banner_shape02.png'className='shape02'  ></img>
     <img src='img/banner_shape03.png'className='shape03' ></img>
@@ -31,8 +42,6 @@ function App() {
     <img src='img/banner_shape05.png'className='shape05' ></img>
     <img src='img/banner_shape06.png'className='shape06' ></img>
     <img src='img/banner_shape07.png'className='shape07' ></img>
-    </div>
-    <div className='b'>
     </div>
     </section>
   );
